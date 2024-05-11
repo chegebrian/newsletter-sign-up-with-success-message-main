c2be41eb10bd8d49304ff75cf0e0a0a029e5bafe
Brian Waithaka <waithakabrian8@gmail.com>
2024-05-11 21:19:33 +0300

    implement error state and success state

--- script.js
+++ script.js
@@ -1,16 +1,32 @@
 const email = document.querySelector("#email");
 const errorEl = document.querySelector(".error");
 const formEl = document.querySelector(".form");
+const wrapperEl = document.querySelector(".wrapper");
+const successEl = document.querySelector(".success");
+const dismissEl = document.querySelector(".dismiss");
+console.log(email);
+
+email.addEventListener("input", () => {
+  if (email.validity.valid) {
+    errorEl.textContent = "";
+  }
+});
 
 //submit the form
 formEl.addEventListener("submit", (e) => {
+  e.preventDefault();
   // if the email field is valid, we let the form submit
+  if (email.validity.valid) {
+    email.value = "";
+    wrapperEl.classList.add("none");
+    successEl.classList.remove("none");
+  }
   if (!email.validity.valid) {
     // If it isn't, we display an appropriate error message
     showError();
   }
   // Then we prevent the form from being sent by canceling the event
-  e.preventDefault();
+  //   e.preventDefault();
 });
 
 // error handling
@@ -25,3 +41,8 @@ function showError() {
     errorEl.textContent = "invalid email address";
   }
 }
+
+dismissEl.addEventListener("click", (e) => {
+  wrapperEl.classList.remove("none");
+  successEl.classList.add("none");
+});
